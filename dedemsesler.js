c8c5fd52894f522f95ed8974ee704073d83cf373
thexehn <133305821+thexehn@users.noreply.github.com>
2023-05-12 06:28:36 +0300

    Add files via upload

---
+++ dedemsesler.js
@@ -0,0 +1,19 @@
+var audio = new Audio('masa.mp3');
+audio.oncanplay = function() {
+if (document.getElementById("oymuhurtikla").checked) this.play()
+}
+function basliyoruzCheck(el) {    
+  if (el.checked) {
+    audio.load();
+    var sounds = [
+        "basliyoruz.mp3",
+         "ben kemal geliyorum.mp3",
+        "6filo.mp3",
+        "enkisavideo.mp3",
+         "kimsepadisahdegil.mp3"];
+      
+     var soundFile = sounds[Math.floor(Math.random()*sounds.length)];
+     document.getElementById("dedemraconlariplayer").innerHTML="<embed src=\""+soundFile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
+  }
+  
+}
