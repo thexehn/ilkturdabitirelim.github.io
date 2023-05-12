0285646c78b72631475964e4ca83ca0f3b924192
thexehn <133305821+thexehn@users.noreply.github.com>
2023-05-12 07:21:48 +0300

    Update dedemsesler.js

--- dedemsesler.js
+++ dedemsesler.js
@@ -1,3 +1,16 @@
+var sounds = [
+  "basliyoruz.mp3",
+  "ben kemal geliyorum.mp3",
+  "6filo.mp3",
+  "enkisavideo.mp3",
+  "kimsepadisahdegil.mp3"
+];
+
+var soundFile = sounds[Math.floor(Math.random()*sounds.length)];
+var audios = new Audio(soundFile);
+audios.autoplay = true;
+
+
 var audio = new Audio('masa.mp3');
 audio.oncanplay = function() {
 if (document.getElementById("oymuhurtikla").checked) this.play()
@@ -5,18 +18,10 @@ if (document.getElementById("oymuhurtikla").checked) this.play()
 function basliyoruzCheck(el) {    
   if (el.checked) {
     audio.load();
-    var sounds = [
-        "basliyoruz.mp3",
-         "ben kemal geliyorum.mp3",
-        "6filo.mp3",
-        "enkisavideo.mp3",
-         "kimsepadisahdegil.mp3"];
-  
-      
-     var soundFile = sounds[Math.floor(Math.random()*sounds.length)];
-     document.getElementById("dedemraconlariplayer").innerHTML="<embed src=\""+soundFile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
-    
+    audios.load();
+    audios.play();
     console.log(soundFile);
   }
+
   
 }
