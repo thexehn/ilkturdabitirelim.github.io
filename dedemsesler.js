ae8c03f68018d56f891bf6d13b11e13e4d740ed3
thexehn <133305821+thexehn@users.noreply.github.com>
2023-05-12 07:46:33 +0300

    Update dedemsesler.js

--- dedemsesler.js
+++ dedemsesler.js
@@ -6,22 +6,42 @@ var sounds = [
   "kimsepadisahdegil.mp3"
 ];
 
-var soundFile = sounds[Math.floor(Math.random()*sounds.length)];
-var audios = new Audio(soundFile);
+var audios = new Audio();
 audios.autoplay = true;
-
 
 var audio = new Audio('masa.mp3');
 audio.oncanplay = function() {
-if (document.getElementById("oymuhurtikla").checked) this.play()
+  if (document.getElementById("oymuhurtikla").checked) this.play()
 }
+
+var count;
+
+window.onload = function() {
+  count = localStorage.getItem("count") || 0;
+  document.getElementById("count").innerHTML = count + "&nbsp;";
+}
+
 function basliyoruzCheck(el) {    
   if (el.checked) {
     audio.load();
+    audios.src = sounds[Math.floor(Math.random()*sounds.length)];
     audios.load();
     audios.play();
-    console.log(soundFile);
+    count++;
+    
+    el.addEventListener('change', function() {
+      if (!this.checked) {
+        audios.pause();
+      }
+    });
+    
+  } else {
+    audios.pause();
   }
+  localStorage.setItem("count", count);
+  document.getElementById("count").innerHTML = count + "&nbsp;";
+}
 
-  
+function refreshPage() {
+  window.location.reload();
 }
