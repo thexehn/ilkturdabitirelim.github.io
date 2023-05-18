59c5f30ee8050cbc5c577bf302e1490c23070ad7
thexehn <133305821+thexehn@users.noreply.github.com>
2023-05-19 02:01:39 +0300

    Update dedemsesler.js

--- dedemsesler.js
+++ dedemsesler.js
@@ -3,7 +3,8 @@ var sounds = [
   "ben kemal geliyorum.mp3",
   "6filo.mp3",
   "enkisavideo.mp3",
-  "kimsepadisahdegil.mp3"
+  "kimsepadisahdegil.mp3",
+  "terlemegokcek.mp3"
 ];
 
 var audios = new Audio();
@@ -11,8 +12,8 @@ audios.autoplay = true;
 
 var audio = new Audio('masa.mp3');
 audio.oncanplay = function() {
-  if (document.getElementById("oymuhurtikla").checked) this.play()
-}
+  if (document.getElementById("oymuhurtikla").checked) this.play();
+};
 
 // Sayfa yüklendiğinde count değerini güncelle
 window.onload = function() {
@@ -20,38 +21,32 @@ window.onload = function() {
   document.getElementById("count").innerHTML = count + "&nbsp;";
 };
 
-function basliyoruzCheck(el) {    
-  if (el.checked) {
-    audio.load();
-    audios.src = sounds[Math.floor(Math.random()*sounds.length)];
-    audios.load();
-    audios.play();
+var checkboxClicked = false;
+
+function basliyoruzCheck(el) {
+  if (!checkboxClicked) {
+    checkboxClicked = true;
+    if (el.checked) {
+      audio.load();
+      audios.src = sounds[Math.floor(Math.random() * sounds.length)];
+      audios.load();
+      audios.play();
+      var count = localStorage.getItem("count") || 0;
+      count++;
+      localStorage.setItem("count", count);
+    } else {
+      audios.pause();
+    }
+    el.disabled = true; // Checkbox'u devre dışı bırak
     var count = localStorage.getItem("count") || 0;
-    count++;
-    localStorage.setItem("count", count);
-    el.addEventListener('change', function() {
-      if (!this.checked) {
-        audios.pause();
-      }
+    document.getElementById("count").innerHTML = count + "&nbsp;";
+    console.log("Çalınan ses dosyası: ", audios.src);
+
+    // Sayfayı ses dosyası tamamlandığında yenile
+    audios.addEventListener("ended", function() {
+      window.location.reload();
     });
-    
   } else {
-    audios.pause();
+    el.checked = false;
   }
-  var count = localStorage.getItem("count") || 0;
-  document.getElementById("count").innerHTML = count + "&nbsp;";
-  console.log("Çalınan ses dosyası: ", audios.src);
-  
-  // Sayfayı ses dosyası tamamlandığında yenile
-  audios.addEventListener("ended", function() {
-    window.location.reload();
-  });
 }
-
-
-
-
-
-
-
-
